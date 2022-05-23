95d572ee2621c09c0c248efebb40ccb9c44d0f39
KingfernJohn <49335210+KingfernJohn@users.noreply.github.com>
2022-05-23 20:53:57 +0200

    Add files via upload

--- download-bot.py
+++ download-bot.py
@@ -1,12 +1,33 @@
-import selenium
-import time, os
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+try:
+    import selenium
+    import time, os, platform
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+except:
+    print()
+    print("ERROR!")
+    print("Error while importing modules. Please install the modules in requirements.txt")
+    input("Press Enter to Exit ")
+    quit()
+
+print("""
+  _____   ______          ___   _ _      ____          _____             ____   ____ _______ 
+ |  __ \ / __ \ \        / / \ | | |    / __ \   /\   |  __ \           |  _ \ / __ \__   __|
+ | |  | | |  | \ \  /\  / /|  \| | |   | |  | | /  \  | |  | |  ______  | |_) | |  | | | |   
+ | |  | | |  | |\ \/  \/ / | . ` | |   | |  | |/ /\ \ | |  | | |______| |  _ <| |  | | | |   
+ | |__| | |__| | \  /\  /  | |\  | |___| |__| / ____ \| |__| |          | |_) | |__| | | |   
+ |_____/ \____/   \/  \/   |_| \_|______\____/_/    \_\_____/           |____/ \____/  |_|                                                                            
+  """)
+
+if platform.system() == "Windows":
+    clear = "cls"
+else:
+    clear = "clear"
 
 print()
-print("DOWNLOAD-BOTTER")
+print("DOWNLOAD-BOT by KingfernJohn")
 print()
 starget = int(input("TARGET-DOWNLOAD.AMOUNT: "))
 stargetadr = input("TARGET-WEB.DOWNLOAD.ADRES: ")
@@ -24,3 +45,5 @@ for x in range(starget):
     time.sleep(10)                    # CHANGE HERE - FASTER BUT COULD CAUSE DATA TRASH
     if os.path.exists(file_path):
         os.remove(file_path)
+time.sleep(15)
+browser.quit()
